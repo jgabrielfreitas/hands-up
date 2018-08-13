b8f131b8fd07d8788b77e1adff9af9e6f7451457
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-08-13 04:06:50 -0300

    add application intro

--- app/src/main/java/com/jgabrielfreitas/handsup/activity/introduction/IntroPresenter.kt
+++ app/src/main/java/com/jgabrielfreitas/handsup/activity/introduction/IntroPresenter.kt
@@ -1,9 +1,29 @@
 package com.jgabrielfreitas.handsup.activity.introduction
 
-class IntroPresenter(var view: IntroContract.View) : IntroContract.Presenter {
+import agency.tango.materialintroscreen.SlideFragment
+import agency.tango.materialintroscreen.SlideFragmentBuilder
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import com.jgabrielfreitas.handsup.R
+import javax.inject.Inject
 
+class IntroPresenter @Inject constructor(val view: IntroContract.View) : IntroContract.Presenter {
+
+    fun getSlides(): List<SlideFragment> {
+
+        val slideFragmentsList = mutableListOf<SlideFragment>()
+
+        val helloSlideFragment = SlideFragmentBuilder().also {
+            it.backgroundColor(R.color.introHelloFragment)
+            it.title("Hello")
+            it.description("Description 1")
+            it.neededPermissions(arrayOf(ACCESS_FINE_LOCATION))
+        }.build()
+        slideFragmentsList.add(helloSlideFragment)
+
+        return slideFragmentsList
+    }
 
     override fun prepareSlides() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        view.addSlidesFragments(getSlides())
     }
 }
--- app/src/main/java/com/jgabrielfreitas/handsup/activity/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/jgabrielfreitas/handsup/activity/introduction/IntroductionActivity.kt
@@ -4,15 +4,23 @@ import com.jgabrielfreitas.handsup.activity.introduction.IntroContract.*
 import agency.tango.materialintroscreen.MaterialIntroActivity
 import agency.tango.materialintroscreen.SlideFragment
 import android.os.Bundle
+import com.jgabrielfreitas.handsup.R
+import com.jgabrielfreitas.handsup.activity.introduction.di.DaggerIntroComponent
+import com.jgabrielfreitas.handsup.activity.introduction.di.IntroPresenterModule
 import javax.inject.Inject
 
 class IntroductionActivity : MaterialIntroActivity(), View {
 
     @Inject
-    private lateinit var presenter: Presenter
+    lateinit var presenter: Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        DaggerIntroComponent.builder()
+                            .introPresenterModule(IntroPresenterModule(this))
+                            .build()
+                            .inject(this)
         presenter.prepareSlides()
     }
 
